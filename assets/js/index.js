567e3659e8270b8ebb7614648d432d19f9df80eb
Galeta Ivan <galsavy3@gmail.com>
2022-09-21 12:45:49 +0300

    pravelnaya

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,31 @@
 "use strict";
 
-function factorial(num){
-    if(num === 0 ){
-        return 1;
+let str = 'test test test testlist test test test';
+
+function getQuantity (str){
+    const words = str.split(' ');
+    let result = 0;
+    for (let i = 0; i < words.length; i++){        
+        if (words [i] != ' '){
+            result += 1
+        }
     }
-    return num * factorial(num - 1) 
+    return result; 
 }
-console.log(factorial(4));
+console.log(getQuantity(str));
+
+
+function getLongWord (str){
+
+    const longWord = str.split(' ');
+    let wordLength = 0;
+    let result;
+    for( let i = 0; i < longWord.length; i++ ){
+        wordLength = longWord.length;
+        if(longWord[i].length > wordLength){
+            result= longWord[i];            
+        }
+    }
+    return result;
+}
+console.log(getLongWord(str));
